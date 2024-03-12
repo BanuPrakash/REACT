0db3d2632feb82217f8b75584b3dcb50b15289fa
Banuprakash <banuprakashc@yahoo.co.in>
2024-03-12 12:56:44 +0530

    day 2

--- trials/src/hooks/EffectExample.jsx
+++ trials/src/hooks/EffectExample.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 
 export default function EffectExample() {
     let [users, setUsers] = useState([]);
+    let [id, setId] = useState(0);
+    let [user, setUser] = useState(null);
 
     // componentDidMount
     useEffect(() => {
@@ -12,15 +14,32 @@ export default function EffectExample() {
             })
     }, []);
 
+    // componentDidUpdate
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users/' + id)
+            .then(response => response.json())
+            .then(data => {
+                setUser(data);
+            })
+    }, [id]);
+
+
     if (users.length === 0) {
         return <h1>Loading....</h1>
     } else {
         return (
             <div>
                 {
-                    users.map(user => <h1 key={user.id}>{user.email}</h1>)
+                    users.map(user => <h1 key={user.id} onMouseEnter={() => setId(user.id)}>{user.email}</h1>)
                 }
+                <div>
+                    <h1>Details</h1>
+                    Company: {user?.company.name} <br />
+                    Website: {user?.website} <br />
+                </div>
+
             </div>
         )
     }
+
 }
