7cbb33133500cc5e07759a5dbc709b690ce8bb91
Banuprakash <banuprakashc@yahoo.co.in>
2024-06-12 12:18:16 +0530

    shopapp

--- shopapp/src/components/ProductCard.tsx
+++ shopapp/src/components/ProductCard.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
-
-export default function ProductCard() {
+import Product from '../model/Product'
+import Card from 'react-bootstrap/Card';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+type AppProps = {
+  product: Product
+}
+export default function ProductCard({ product }: AppProps) {
   return (
-    <div>ProductCard</div>
+    <div className='col-sm-6 col-md-4 mb-2'>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={product.image} />
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>
+            {product.description}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          Rs.{product.price}
+          <span className='right px-2'>
+            <FontAwesomeIcon icon={faHeart} color='red' />
+            <FontAwesomeIcon icon={faShoppingCart} color='blue' />
+          </span>
+        </Card.Footer>
+      </Card>
+    </div>
   )
 }
--- shopapp/src/components/ProductList.tsx
+++ shopapp/src/components/ProductList.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Product from '../model/Product'
+import axios from 'axios';
+import { Container } from 'react-bootstrap';
+import ProductCard from './ProductCard';
 
 export default function ProductList() {
+  let [products, setProducts] = useState<Product[]>();
+  
+  // componentDidMount
+  useEffect(() => {
+    axios.get("https://fakestoreapi.com/products?limit=5").then(response => {
+      setProducts(response.data);
+    });
+  }, []);
+
   return (
-    <div>ProductList</div>
+    <Container>
+      <div className='row'>
+        {
+          products && products.map(product => <ProductCard key={product.id} product={product} />)
+        }
+      </div>
+    </Container>
   )
 }
