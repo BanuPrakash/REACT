1d4601c4393f506358949cbcf21712fe4f236477
Banuprakash <banuprakashc@yahoo.co.in>
2025-06-11 14:45:19 +0530

    ecom app

--- codes/productapp/src/App.js
+++ codes/productapp/src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
+import React, { lazy, Suspense } from 'react'
+
 import './App.css';
 import { Container } from 'react-bootstrap'
 import NavbarComponent from './components/NavbarComponent'
 import { Route, Routes } from 'react-router-dom';
 
 import ProductList from './components/ProductList'
-import ProductForm from './components/ProductForm'
-import CartComponent from './components/CartComponent'
 import Default from './components/Default'
-import Details from './components/Details'
+import ProductForm from './components/ProductForm'
+
+// Lazy loaded 
+const CartComponent = React.lazy(() => import('./components/CartComponent'))
+const Details = lazy(() => import('./components/Details'));
 
 function App() {
   return (
@@ -16,7 +20,11 @@ function App() {
       <NavbarComponent />
       <Routes>
         <Route path='/products' element={<ProductList />} />
-        <Route path='/cart' element={<CartComponent />} />
+        <Route path='/cart' element={
+          <Suspense fallback={<div>Loading Cart...</div>}>
+            <CartComponent />
+          </Suspense>
+        } />
         <Route path='/form' element={<ProductForm />} />
         <Route path='/details/:id' element={<Details />} />
         <Route path='/' element={<ProductList />} />
